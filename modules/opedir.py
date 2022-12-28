f0ad4e8becc0d15809fa5b90469ce43548e174da
RyomaIto7 <k21013kk@aitech.ac.jp>
2022-12-28 12:41:44 +0900

    update split
    ファイル操作を簡略化
    ファイルの場所を指定せずにprocessing に存在する処理を適用したい画像を選択しての分割処理が可能

--- modules/opedir.py
+++ modules/opedir.py
@@ -1,19 +1,28 @@
 import os
 
-def current_path():
+'''
+実行場所の違いによるファイル操作のエラーを減らすために現在のディレクトリと親ディレクトリの絶対パスを取得する関数
+'''
+
+def show_cur_pare_path():
     base = os.path.dirname(os.path.abspath(__file__))
     name = os.path.normpath(os.path.join(base, '..'))
     print(base)
     print(name)
-
+#カレントディレクトリを取得
 def get_current_path():
     base = os.path.dirname(os.path.abspath(__file__))
     return base
-
+#親ディレクトリ(一つ上のディレクトリ)を取得
 def get_parent_path():
     base = get_current_path()
     name = os.path.normpath(os.path.join(base, '..'))
     return name
+
+
+
+
+
 
 
 
@@ -29,5 +38,5 @@ if __name__ == "__main__":
     print(base)
     print(name)
     print()
-    current_path()
+    show_cur_pare_path()
     print(get_current_path())
--- modules/split.py
+++ modules/split.py
@@ -2,7 +2,6 @@ from xml.etree.ElementPath import get_parent_map
 import cv2
 import numpy as np
 import os
-
 from opedir import get_current_path , get_parent_path
 
 
@@ -13,13 +12,16 @@ from opedir import get_current_path , get_parent_path
 #画像の読み込み
 #photo=cv2.imread('sample.jpg',cv2.IMREAD_COLOR)
 
-def split_img(input_img, output_img,rows,cols):
+def split_img(input,rows,cols):
+    input_img = get_parent_path()+'/static/images/process/'+input
+    #print(input_img)
     img=cv2.imread(input_img)
     chunks = []
     for row_img in np.array_split(img, rows, axis=0):
         for chunk in np.array_split(row_img, cols, axis=1):
             chunks.append(chunk)
     print(len(chunks))
+    output_img = get_parent_path() + '/static/images/split/'
     for i, chunk in enumerate(chunks):
         cv2.imwrite(output_img+f"chunk_{i:02d}.png",chunk)
     
@@ -30,10 +32,10 @@ if __name__ == "__main__":
     input = './static/images/process/sample.png'
     output = './static/images/split/'
     images = 'sample.png'
-    print(get_current_path())
-    print(get_parent_path())
+    #print(get_current_path())
+    #print(get_parent_path())
 
     rows=6
     cols=6
-    #split_img(input,output,6,6)
+    split_img(images,6,6)
     
