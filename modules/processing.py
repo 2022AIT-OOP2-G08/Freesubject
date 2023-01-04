8e3d13aebdb45fd1ac91b7a78840e3933fbe6340
Sharu <104481020+SakaiHaruto@users.noreply.github.com>
2023-01-04 12:34:04 +0900

    パス取得の関数と組み合わせる

--- modules/processing.py
+++ modules/processing.py
@@ -1,22 +1,55 @@
+import time
 import cv2
 import numpy as np
+import os
 
+#パス取得関数
+#ファイル内でのディレクトリ構造の統一のためにカレントディレクトリと親ディレクトリの位置を取得する関数を導入
+def show_cur_pare_path():
+    base = os.path.dirname(os.path.abspath(__file__))
+    name = os.path.normpath(os.path.join(base, '..'))
+    print(base)
+    print(name)
+#カレントディレクトリの絶対パスを取得
+def get_current_path():
+    base = os.path.dirname(os.path.abspath(__file__))
+    return base
+#親ディレクトリ(一つ上のディレクトリ)の絶対パスを取得(module内の関数ならFreeSubjectまでのパスを取得)
+def get_parent_path():
+    base = get_current_path()
+    name = os.path.normpath(os.path.join(base, '..'))
+    return name
+
+#images のパスを取得
+def get_images_path():
+    name = get_parent_path()+'/static/images/'
+    return name
+
+#images内 のパスを取得
+def get_img_select_path(imgtype):
+    name = get_images_path()+imgtype+'/'
+    return name
+
+#画像処理関数
 def gray_scale(input): #グレースケール化
-    img_bgr = cv2.imread(input)
+    img = get_img_select_path('normal')+input
+    img_bgr = cv2.imread(img)
     img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
     cv2.imshow('gray_img',img_gray)
     cv2.waitKey(0)
 
 
 def mosaic(input, ratio=0.005): #モザイク化
-    img_bgr = cv2.imread(input) 
+    img = get_img_select_path('normal')+input
+    img_bgr = cv2.imread(img) 
     small = cv2.resize(img_bgr, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
     img_mosaic =  cv2.resize(small, img_bgr.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
     cv2.imshow('mosaic_img',img_mosaic)
     cv2.waitKey(0)
 
 def line(input): #線画化
-    gray = cv2.imread(input, cv2.IMREAD_GRAYSCALE)
+    img = get_img_select_path('normal')+input
+    gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     neiborhood24 = np.array([[1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1],
@@ -31,8 +64,9 @@ def line(input): #線画化
     cv2.waitKey(0)
 
 def changecolor(input): #色変化
-    img = cv2.imread(input)
-    img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV) # 色空間をBGRからHSVに変換
+    img = get_img_select_path('normal')+input
+    img_bgr = cv2.imread(img)
+    img_hsv = cv2.cvtColor(img_bgr,cv2.COLOR_BGR2HSV) # 色空間をBGRからHSVに変換
     h_deg = 50 #色相(Hue)の回転度数
     s_mag = 2.0 # 彩度(Saturation)の倍率
     v_mag = 1 # 明度(Value)の倍率
@@ -45,8 +79,9 @@ def changecolor(input): #色変化
     cv2.waitKey(0)
 
 def inversion(input): #色反転
-    img = cv2.imread(input)
-    img_inv = cv2.bitwise_not(img)
+    img = get_img_select_path('normal')+input
+    img_bgr = cv2.imread(img)
+    img_inv = cv2.bitwise_not(img_bgr)
     cv2.imshow('inversion_ing',img_inv)
     cv2.waitKey(0)
 
@@ -54,7 +89,7 @@ def inversion(input): #色反転
 
 #debug用コード
 if __name__ == "__main__":
-    input = 'static/images/normal/kinopio.jpeg'
+    input = 'kinopio.jpeg'
     # gray_scale(input)
     # mosaic(input)
     # line(input)
