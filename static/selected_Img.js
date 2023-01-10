2c0480752c44df30f9f618261a697f74cf2d3062
OtaKeisuke <meronn0818@gmail.com>
2023-01-11 03:52:40 +0900

    decideImg()内の処理を変更
    ・画像が選択された際にもし決定ボタンのdisabledがtrueだった場合はfalseにして押せるようにする処理を追加。
    ・選択されたラジオボタンのidを取得してクリエパラメータ用に使うinput[type="hidden"]のvalueに代入する処理を追加。

--- static/selected_Img.js
+++ static/selected_Img.js
@@ -1,17 +1,24 @@
 function decideImg() {
-    var flag = false; // 選択されているか否かを判定するフラグ
+    // var flag = false; // 選択されているか否かを判定するフラグ
+
+    //決定ボタンのdisabledがtrueだったらfalseにして押せるようにする
+    if(document.decide.decide_button.disabled){
+        document.decide.decide_button.disabled = false
+    }
    
     //　ラジオボタンの数だけ判定を繰り返す
     for(var i=0; i<document.imageBoxes.img.length;i++){
-  
         // i番目のラジオボタンがチェックされているかを判定
         if(document.imageBoxes.img[i].checked){ 
-            flag = true;    
-            alert(document.imageBoxes.img[i].id + "が選択されました。");
+            // flag = true;    
+            document.decide.img_Name.value = document.imageBoxes.img[i].id
+            console.log(document.imageBoxes.img[i].id + "が選択されました。")
+            // alert(document.imageBoxes.img[i].id + "が選択されました。");
         }
     }
     // 何も選択されていない場合の処理
-    if(!flag){ 
-        alert("画像が選択されていません。");
-    } 
+    // if(!flag){ 
+    //     console.log("画像が選択されていません。")
+    //     alert("画像が選択されていません。");
+    // } 
 }
