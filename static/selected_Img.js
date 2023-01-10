8481e81ac1fe78983b8d2a262a34d85aaeda426a
OtaKeisuke <meronn0818@gmail.com>
2023-01-11 01:50:05 +0900

    選択されている画像のファイル名を取得して表示する関数を作成

--- static/selected_Img.js
+++ static/selected_Img.js
@@ -1,6 +1,17 @@
-function changeFlame(id) {
-    
-    //ここに処理を書いていく
-    console.log("aaaa")
-    // let pAndspan_create = document.querySelectorAll('.row ');
+function decideImg() {
+    var flag = false; // 選択されているか否かを判定するフラグ
+   
+    //　ラジオボタンの数だけ判定を繰り返す
+    for(var i=0; i<document.imageBoxes.img.length;i++){
+  
+        // i番目のラジオボタンがチェックされているかを判定
+        if(document.imageBoxes.img[i].checked){ 
+            flag = true;    
+            alert(document.imageBoxes.img[i].id + "が選択されました。");
+        }
+    }
+    // 何も選択されていない場合の処理
+    if(!flag){ 
+        alert("画像が選択されていません。");
+    } 
 }
