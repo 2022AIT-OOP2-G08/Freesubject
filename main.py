3ba7ea744e716e2e5dfada47a973cca29da43a68
u-ryusei <k20233kk@aitech.ac.jp>
2023-01-19 14:32:22 +0900

    パズルピースのランダム配置

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify, send_from_directory
 import json  # Python標準のJSONライブラリを読み込んで、データの保存等に使用する
 import glob  # ファイルの一覧を取得用に使用
 import os  # パス操作用に使用
+import random
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
