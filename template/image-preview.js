0aa2ed05bb3e27fcfd780c737c9fbc3b1581bb37
Sharu <104481020+SakaiHaruto@users.noreply.github.com>
2023-01-19 09:41:25 +0900

    不要ファイルの削除、template->templates

--- template/image-preview.js
+++
@@ -1,4 +0,0 @@
-$('.image').children('img').attr('src', '../static/images/normal/sample.png');
-$('button').on('click', function(){
-    $('.image').children('img').attr('src', '//into-the-program.com/demo/images/sample002.jpg');
-});
